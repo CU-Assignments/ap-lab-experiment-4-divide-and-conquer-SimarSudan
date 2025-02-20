4cfbe56f9acb61ed091391c9990eefaf9216f513
Manvir Singh <145987787+SimarSudan@users.noreply.github.com>
2025-02-20 15:11:43 +0530

    Create 704.java

---
+++ 704.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Avoids overflow
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Search right half
+            } else {
+                right = mid - 1; // Search left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+}
+/*Input
+nums =
+[-1,0,3,5,9,12]
+target =
+9
+Output
+4
+Expected
+4
+*/
